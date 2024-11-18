f97a7453edfb2ab2a81269868bbec6add241c621
mandich27 <mandich27@gmail.com>
2024-11-18 11:50:08 +0200

    goit-algo-hw-05

---
+++ calculatorin_text.py
@@ -0,0 +1,23 @@
+from typing import Callable
+import re
+
+
+def generator_numbers(text: str):
+    for match in re.findall(r'\d+\.\d+', text):                             # Шукаємо співпадіння через регулярний вираз числа розділені крапкою
+        yield float(match)
+
+
+def sum_profit(text: str, func: Callable[[str], float])-> float:            # Приймаєм str і перетворюємо його в число float
+    nums = 0
+    for numbers in func(text):                                              # Циклом for перебираєм numbers і додаємо в nums
+        nums += numbers
+    return nums                                                             # Повертаєм nums
+
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+
+
+
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):         # Перевірка чи число додатнє, якщо ні, повертаєм 0 
+        if n <= 0:
+            return   0
+        if n == 1:            # Перевірка чи число 1 в такому випадку повертаєм 1 
+            return 1
+        if n  in cache:       # Перевірка чи число є в кеші, якщо так то виводимо число з кешу без обрахунків
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)          # Обчислюємо число Фібоначчі додаючи два попередні 
+        return cache[n]                                         # Повертаєм обчислене число 
+    return fibonacci                                            # додаючи його в словник cache
+
+
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
